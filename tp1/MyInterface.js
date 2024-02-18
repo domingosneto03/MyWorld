d2d574bf9b39f92199eca511a9ed3f0d37ee9d35
José Caribé <up202103344@edu.fe.up.pt>
2024-02-18 19:54:36 +0000

    Exercício 1

--- tp1/MyInterface.js
+++ tp1/MyInterface.js
@@ -23,6 +23,10 @@ export class MyInterface extends CGFinterface {
         //Slider element in GUI
         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
+        this.gui.add(this.scene, 'displayDiamond').name('Diamond');
+        this.gui.add(this.scene, 'displayTriangle').name('Triangle');
+        this.gui.add(this.scene, 'displayParallelogram').name('Parallelogram');
+
         return true;
     }
 }
